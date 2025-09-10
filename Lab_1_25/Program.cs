efd82b11ba5fa00f664bf67156347fb2887e403f
Svetlana184 <sveta.kgd.39@gmail.com>
2025-09-10 16:40:55 +0200

    выборка

--- Lab_1_25/Program.cs
+++ Lab_1_25/Program.cs
@@ -1,13 +1,26 @@
 ﻿using Lab_1_25;
 using Lab_1_25.Models;
 
-using (SapunovaDemoContext db =  new SapunovaDemoContext())
+//using (SapunovaDemoContext db =  new SapunovaDemoContext())
+//{
+//    Company c1 = new Company();
+//    c1.Name = "рога без копыт";
+//    db.Companies.Add(c1);
+//    Company c2 = new Company();
+//    c2.Name = "копыта без рогов";
+//    db.Companies.Add(c2);
+//    db.SaveChanges();
+//}
+
+using (SapunovaDemoContext db = new SapunovaDemoContext())
 {
-    Company c1 = new Company();
-    c1.Name = "рога без копыт";
-    db.Companies.Add(c1);
-    Company c2 = new Company();
-    c2.Name = "копыта без рогов";
-    db.Companies.Add(c2);
-    db.SaveChanges();
+    List<Company> companies = db.Companies.ToList();
+    foreach (Company company in companies) Console.WriteLine(company.IdCompany + " " + company.Name);
+
+}
+
+using (SapunovaDemoContext db = new SapunovaDemoContext())
+{
+    List<string> companiesNames = db.Companies.Select(p=>p.Name).ToList();
+    foreach(string name in companiesNames) Console.WriteLine(name);
 }
